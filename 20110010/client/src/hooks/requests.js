ff0af529105fce68f8119d9eda5726fe4a468e27
Nguyễn Hoàng Nhân <nguyen.hngnhan21@gmail.com>
2023-10-24 17:55:28 +0700

    Nasa project

---
+++ 20110010/client/src/hooks/requests.js
@@ -0,0 +1,39 @@
+const API_URL = "http://localhost:8000";
+async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
+}
+
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`);
+  const fectchedLaunches = await response.json();
+  return fectchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+}
+
+async function httpSubmitLaunch(launch) {
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (error) {
+    return { ok: false, message: error.message };
+  }
+}
+
+async function httpAbortLaunch(id) {
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch (error) {
+    return { ok: false, message: error.message };
+  }
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
